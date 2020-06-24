a0344541f33b6ee19200f93c96805510dfc2679a
aleradamantis99 <31964732+aleradamantis99@users.noreply.github.com>
2020-06-24 17:56:43 +0100

    Update main.cpp
    Fixed format

--- main.cpp
+++ main.cpp
@@ -2,22 +2,22 @@
 #include <iostream>
 int main()
 {
-	Board t(3, 4);
+        Board t(3, 4);
 	
-	t.replace_cell(2, 1, 1);
-	t.replace_cell(1, 0, 1);
-	t.replace_cell(1, 1, 3);
-    t.replace_cell(3, 2, 2);
-    t.replace_cell(3, 2, 3);
-    t.replace_cell(2, 1, 0);
+        t.replace_cell(2, 1, 1);
+        t.replace_cell(1, 0, 1);
+        t.replace_cell(1, 1, 3);
+        t.replace_cell(3, 2, 2);
+        t.replace_cell(3, 2, 3);
+        t.replace_cell(2, 1, 0);
 
 	t.show(std::cout);
 	std::cout << '\n';
 	for (int i=0; i<5; ++i)
 	{
-		t.round();
-		t.show(std::cout);
-		std::cout << '\n';
+                t.round();
+                t.show(std::cout);
+                std::cout << '\n';
 	}
 	return 0;
 }
